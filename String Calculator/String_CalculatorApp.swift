f85a0396e22e08330dfdcd29bfd03dd6727c70c3
uday <udaygajera116@gmail.com>
2025-06-10 16:28:51 +0530

    Initial Commit

---
+++ String Calculator/String_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  String_CalculatorApp.swift
+//  String Calculator
+//
+//  Created by Uday Gajera on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct String_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

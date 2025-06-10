6c2e4746c9a488e56fbfc04a41f5d5f187ac99ce
uday <udaygajera116@gmail.com>
2025-06-10 19:45:31 +0530

    Handle: custom delimiter

--- String Calculator/Calculator.swift
+++ String Calculator/Calculator.swift
@@ -13,7 +13,18 @@ class Calculator {
         guard !numbers.isEmpty else {
             return 0
         }
-        let parts = numbers.components(separatedBy: CharacterSet(charactersIn: ",\n"))
+        var delimiter = ","
+        var numberString = numbers
+        if numberString.hasPrefix("//") {
+            let parts = numberString.components(separatedBy: "\n")
+            if parts.count >= 2 {
+                delimiter = String(parts[0].dropFirst(2))
+                numberString = parts[1...].joined(separator: "\n")
+            }
+        }
+        let parts = numberString.replacingOccurrences(of: "\n", with: delimiter)
+                                .components(separatedBy: delimiter)
+        
         return parts.compactMap { Int($0.trimmingCharacters(in: .whitespaces)) }.reduce(0, +)
     }
 }

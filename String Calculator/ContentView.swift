027146857cfa5322a14ccc61670314482d375752
uday <udaygajera116@gmail.com>
2025-06-11 15:17:18 +0530

    Refactor: modularise main view for better readability

--- String Calculator/ContentView.swift
+++ String Calculator/ContentView.swift
@@ -9,52 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var inputText: String = ""
-    @State private var additionResult: Int = 0
+    @State private var additionResult: Int?
     @State private var errorMessage: String = ""
     private let calculator = Calculator()
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("String Calculator")
+        VStack(alignment: .center, spacing: 20) {
+            
+            HeadlineTextView
+            
+            TextEditorView
+                .font(.title3)
+                .fontWeight(.regular)
+            
+            CalculateButtonView
                 .font(.title)
-                .padding()
+                .fontWeight(.regular)
             
-            TextEditor(text: $inputText)
-                .border(Color.gray, width: 2)
-                .frame(maxHeight: 100)
-                .padding()
-            
-            Button("Calculate Sum") {
-                errorMessage = ""
-                do {
-                    additionResult = try calculator.add(inputText)
-                } catch let error as CalculatorError {
-                    errorMessage = error.localizedDescription
-                    additionResult = 0
-                } catch {
-                    errorMessage = "Unexpected error occurred"
-                    additionResult = 0
-                }
-            }
+            ResultView
+        }
+    }
+}
+extension ContentView {
+    
+    private var HeadlineTextView: some View {
+        Text("String Calculator")
+            .font(.largeTitle)
+            .fontWeight(.medium)
             .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(8)
-            
+    }
+    
+    private var TextEditorView: some View {
+        TextEditor(text: $inputText)
+            .border(Color.gray, width: 2)
+            .frame(maxHeight: 100)
+            .padding()
+    }
+    
+    private var CalculateButtonView: some View {
+        Button("Calculate Sum") {
+            errorMessage = ""
+            do {
+                additionResult = try calculator.add(inputText)
+            } catch let error as CalculatorError {
+                errorMessage = error.localizedDescription
+                additionResult = 0
+            } catch {
+                errorMessage = "Unexpected error occurred"
+                additionResult = 0
+            }
+        }
+        .padding()
+        .background(Color.blue)
+        .buttonStyle(.borderedProminent)
+        .foregroundColor(.white)
+        .cornerRadius(20)
+    }
+    
+    private var ResultView: some View {
+        Group {
             if !errorMessage.isEmpty {
                 Text(errorMessage)
                     .foregroundColor(.red)
                     .padding()
-            } else {
-                Text("Result: \(additionResult)")
+            } else if let res = additionResult {
+                Text("Result: \(res)")
                     .font(.headline)
                     .padding()
             }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
